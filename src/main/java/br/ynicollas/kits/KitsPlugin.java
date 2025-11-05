70db357c02eeae2d6d05c079b46fe7e7f3d6f99f
nicollasgtb <nicolas2006.capela@gmail.com>
2025-11-05 16:44:44 -0300

    The old way of registering commands and listeners has been removed.

--- src/main/java/br/ynicollas/kits/KitsPlugin.java
+++ src/main/java/br/ynicollas/kits/KitsPlugin.java
@@ -2,11 +2,15 @@ package br.ynicollas.kits;
 
 import br.ynicollas.kits.cache.CooldownsCache;
 import br.ynicollas.kits.cache.KitsCache;
-import br.ynicollas.kits.registry.CommandRegistry;
-import br.ynicollas.kits.registry.ListenerRegistry;
+import br.ynicollas.kits.commands.*;
+import br.ynicollas.kits.listeners.InventoryClickListener;
+import br.ynicollas.kits.listeners.InventoryCloseListener;
+import br.ynicollas.kits.listeners.PlayerQuitListener;
 import br.ynicollas.kits.storage.Database;
 import br.ynicollas.kits.storage.cooldowns.CooldownsStorage;
 import br.ynicollas.kits.storage.kits.KitsStorage;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class KitsPlugin extends JavaPlugin {
@@ -16,12 +20,20 @@ public class KitsPlugin extends JavaPlugin {
     private CooldownsStorage cooldowns;
     private KitsStorage kits;
 
+    private CooldownsCache cooldownsCache;
+
     @Override
     public void onEnable() {
+        getLogger().info("Iniciando Kits v" + getDescription().getVersion() + "...");
+
         saveDefaultConfig();
 
-        initializeStorages();
-        registerComponents();
+        setupDatabase();
+
+        registerCommands();
+        registerListeners();
+
+        getLogger().info("Plugin Kits v" + getDescription().getVersion() + " habilitado com sucesso!");
     }
 
     @Override
@@ -29,22 +41,48 @@ public class KitsPlugin extends JavaPlugin {
         if (database != null) {
             database.closeConnection();
         }
+
+        getLogger().info("Plugin Kits desabilitado.");
     }
 
-    private void initializeStorages() {
+    private void setupDatabase() {
         database = new Database(this);
 
         database.openConnection();
 
-        CooldownsCache cooldownsCache = new CooldownsCache();
+        this.cooldownsCache = new CooldownsCache();
         KitsCache kitsCache = new KitsCache();
 
-        cooldowns = new CooldownsStorage(database, cooldownsCache);
-        kits = new KitsStorage(database, kitsCache);
+        this.cooldowns = new CooldownsStorage(database, cooldownsCache);
+        this.kits = new KitsStorage(database, kitsCache);
     }
 
-    private void registerComponents() {
-        new CommandRegistry(this, cooldowns, kits).registerCommands();
-        new ListenerRegistry(this, kits).registerListeners();
+    private void registerCommands() {
+        getLogger().info("Registrando comandos...");
+
+        registerCommand("createkit", new CreateKitCommand(kits));
+        registerCommand("deletekit", new DeleteKitCommand(cooldowns, kits));
+        registerCommand("editkit", new EditKitCommand(kits));
+        registerCommand("givekit", new GiveKitCommand(kits));
+        registerCommand("kit", new KitCommand(cooldowns, kits));
+        registerCommand("viewkit", new ViewKitCommand(kits));
+    }
+
+    private void registerListeners() {
+        getLogger().info("Registrando listeners...");
+
+        getServer().getPluginManager().registerEvents(new InventoryCloseListener(kits), this);
+        getServer().getPluginManager().registerEvents(new InventoryClickListener(), this);
+        getServer().getPluginManager().registerEvents(new PlayerQuitListener(cooldownsCache), this);
+    }
+
+    private void registerCommand(String commandName, CommandExecutor executor) {
+        PluginCommand command = getCommand(commandName);
+
+        if (command != null) {
+            command.setExecutor(executor);
+        } else {
+            getLogger().severe("Falha ao registrar o comando '" + commandName + "'.");
+        }
     }
 }
